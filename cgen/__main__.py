0f6847f502dfcb3ad66dff52b5b3e0efe66b38c9
armannik13 <alevtina9876123@gmail.com>
2025-12-04 11:07:18 +0300

    Update __main__.py

--- cgen/__main__.py
+++ cgen/__main__.py
@@ -84,6 +84,10 @@ def main() -> None:
     help="inject dylibs into @executable_path instead of @executable_path/Frameworks"
   )
   parser.add_argument(
+    "-j", "--custom-path", action="store_true",
+    help="use custom tweaks path"
+  )
+  parser.add_argument(
     "-r", "--patch-plugins", action="store_true",
     help="patch plugins"
   )
